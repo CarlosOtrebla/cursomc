75a65079dffb41ab895dd5c4343e56587f45e7c1
Carlos Alberto Lopes Duarte <carlosotrebla@gmail.com>
2023-08-23 22:46:15 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/carlosotrebla/cursomc/CursomcApplication.java
+++ src/main/java/com/carlosotrebla/cursomc/CursomcApplication.java
@@ -1,14 +1,32 @@
 package com.carlosotrebla.cursomc;
 
-import com.carlosotrebla.cursomc.domain.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import com.carlosotrebla.cursomc.domain.Categoria;
+import com.carlosotrebla.cursomc.domain.Cidade;
+import com.carlosotrebla.cursomc.domain.Cliente;
+import com.carlosotrebla.cursomc.domain.Endereco;
+import com.carlosotrebla.cursomc.domain.Estado;
+import com.carlosotrebla.cursomc.domain.Pagamento;
+import com.carlosotrebla.cursomc.domain.PagamentoComBoleto;
+import com.carlosotrebla.cursomc.domain.PagamentoComCartao;
+import com.carlosotrebla.cursomc.domain.Pedido;
+import com.carlosotrebla.cursomc.domain.Produto;
+import com.carlosotrebla.cursomc.domain.enums.EstadoPagamento;
 import com.carlosotrebla.cursomc.domain.enums.TipoCliente;
-import com.carlosotrebla.cursomc.repositories.*;
+import com.carlosotrebla.cursomc.repositories.CategoriaRepository;
+import com.carlosotrebla.cursomc.repositories.CidadeRepository;
+import com.carlosotrebla.cursomc.repositories.ClienteRepository;
+import com.carlosotrebla.cursomc.repositories.EnderecoRepository;
+import com.carlosotrebla.cursomc.repositories.EstadoRepository;
+import com.carlosotrebla.cursomc.repositories.PagamentoRepository;
+import com.carlosotrebla.cursomc.repositories.PedidoRepository;
+import com.carlosotrebla.cursomc.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
@@ -25,6 +43,11 @@ public class CursomcApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -74,5 +97,20 @@ public class CursomcApplication implements CommandLineRunner {
 
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null );
+		ped2.setPagamento(pagto2);
+
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+
 	}
 }
--- src/main/java/com/carlosotrebla/cursomc/domain/Cliente.java
+++ src/main/java/com/carlosotrebla/cursomc/domain/Cliente.java
@@ -1,5 +1,6 @@
 package com.carlosotrebla.cursomc.domain;
 
+
 import com.carlosotrebla.cursomc.domain.enums.TipoCliente;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
@@ -27,10 +28,14 @@ public class Cliente implements Serializable {
     @CollectionTable(name = "TELEFONE")
     private Set<String> telefones = new HashSet<>();
 
+    @OneToMany(mappedBy = "cliente")
+    private List<Pedido> pedidos = new ArrayList<>();
+
     public Cliente() {
     }
 
     public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
+        super();
         this.id = id;
         this.nome = nome;
         this.email = email;
@@ -94,6 +99,13 @@ public class Cliente implements Serializable {
         this.telefones = telefones;
     }
 
+    public List<Pedido> getPedidos() {
+        return pedidos;
+    }
+    public void setPedidos(List<Pedido> pedidos) {
+        this.pedidos = pedidos;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -105,4 +117,6 @@ public class Cliente implements Serializable {
     public int hashCode() {
         return Objects.hash(getId());
     }
+
+
 }
